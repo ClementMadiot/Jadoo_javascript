2caf145252e7ccf35819225c9e15187553eeda29
ClementMadiot <clementmadiot09@gmail.com>
2024-05-10 16:09:01 +0700

    better class name + add scrollReveal() to header / service / destination

--- scrypt.js
+++ scrypt.js
@@ -2,6 +2,14 @@ const menuBtn = document.getElementById('menuBtn')
 const navLinks = document.getElementById('navLinks')
 const menuBtnIcon = menuBtn.querySelector('i')
 
+window.sr = new ScrollReveal()
+
+const ScrollRevealOption = {
+  distance: '50px',
+  origin: 'bottom',
+  duration: 1000,
+}
+
 menuBtn.addEventListener('click', (e) => {
   navLinks.classList.toggle('open')
 
@@ -12,4 +20,36 @@ menuBtn.addEventListener('click', (e) => {
     navLinks.classList.remove('open')
     menuBtnIcon.setAttribute('class', 'ri-menu-line')
   })
+})
+
+//: Header 
+sr.reveal('.header_image img', {
+  ...ScrollRevealOption,
+  origin: 'right',
+})
+sr.reveal('.header_presentation h3', {
+  ...ScrollRevealOption,
+  delay: 400,
+})
+sr.reveal('h1', {
+  ...ScrollRevealOption,
+  delay: 700,
+})
+sr.reveal('.header_presentation p', {
+  ...ScrollRevealOption,
+  delay: 900,
+})
+sr.reveal('.header_btns', {
+  ...ScrollRevealOption,
+  delay: 1100,
+})
+//: Service 
+sr.reveal('.service_card', {
+  ...ScrollRevealOption,
+  interval: 300,
+})
+//: Destination 
+sr.reveal('.destination_card', {
+  ...ScrollRevealOption,
+  interval: 300,
 })
