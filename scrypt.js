43248c9a3059643f0d58cf4846ec949a9afff8e2
ClementMadiot <clementmadiot09@gmail.com>
2024-05-11 16:46:04 +0700

    rezise img + add function section

--- scrypt.js
+++ scrypt.js
@@ -1,6 +1,10 @@
 const menuBtn = document.getElementById('menuBtn')
 const navLinks = document.getElementById('navLinks')
 const menuBtnIcon = menuBtn.querySelector('i')
+// display section
+const serviceGrid = document.querySelector('.service_grid')
+const destinationGrid = document.querySelector('.destination_grid')
+const tripList = document.querySelector('.trip_list')
 
 window.sr = new ScrollReveal()
 
@@ -9,6 +13,8 @@ const ScrollRevealOption = {
   origin: 'bottom',
   duration: 1000,
 }
+
+//** Toggle Nav  **/
 
 menuBtn.addEventListener('click', (e) => {
   navLinks.classList.toggle('open')
@@ -22,7 +28,146 @@ menuBtn.addEventListener('click', (e) => {
   })
 })
 
-//: Header 
+//** Display Section **/
+
+const serviceArray = [
+  {
+    id: 1,
+    title: 'calculator weather',
+    desc: "Stay ahead of your travels with precise weather forecasts tailored to your destination's conditions.",
+    img: 'assets/weather.png',
+    alt: 'service of weather',
+  },
+  {
+    id: 2,
+    title: 'best flights',
+    desc: "Stay ahead of your travels with precise weather forecasts tailored to your destination's conditions.",
+    img: 'assets/plane.png',
+    alt: 'service of plane',
+  },
+  {
+    id: 3,
+    title: 'local events',
+    desc: "Stay ahead of your travels with precise weather forecasts tailored to your destination's conditions.",
+    img: 'assets/event.png',
+    alt: 'service of event',
+  },
+  {
+    id: 4,
+    title: 'customization',
+    desc: "Stay ahead of your travels with precise weather forecasts tailored to your destination's conditions.",
+    img: 'assets/customisation.png',
+    alt: 'service of customisation',
+  },
+]
+
+function displayService(service) {
+  let displayserviceArray = service
+    .map(function (item) {
+      return `    
+      <div id='${item.id}' class="service_card">
+        <img src="${item.img}" alt="${item.alt}" />
+        <h4>${item.title}</h4>
+        <p>${item.desc}</p>
+      </div>
+`
+    })
+    .join('')
+  serviceGrid.innerHTML = displayserviceArray
+}
+displayService(serviceArray)
+
+const destinationArray = [
+  {
+    id: 4,
+    title: 'Rome, Italy',
+    price: '$5.42k',
+    duration: '10 Days Trip',
+    img: 'assets/destination-1.jpg',
+  },
+  {
+    id: 5,
+    title: 'London, UK',
+    price: '$4.2k',
+    duration: '12 Days Trip',
+    img: 'assets/destination-2.jpg',
+  },
+  {
+    id: 6,
+    title: 'Full, Europe',
+    price: '$15k',
+    duration: '28 Days Trip',
+    img: 'assets/destination-3.jpg',
+  },
+]
+function displayDestination(destination) {
+  let displaydestinationArray = destination
+    .map(function (item) {
+      return `
+    <article id='${item.id}' class="destination_card">
+          <img src="${item.img}" alt="${item.title}" />
+          <div class="destination_details">
+            <div>
+              <h4>${item.title}</h4>
+              <h5>${item.price}</h5>
+            </div>
+            <p>
+              <span><i class="ri-navigation-fill"></i></span>
+              ${item.duration}
+            </p>
+          </div>
+        </article>
+    `
+    })
+    .join('')
+  destinationGrid.innerHTML = displaydestinationArray
+}
+displayDestination(destinationArray)
+
+const tripArray = [
+  {
+    id: 7,
+    icon: 'ri-signpost-line',
+    title: 'choose destination',
+    desc: 'Choose your dream destination from our curated selection of exotic locales, bustling cities and serene getaways.',
+  },
+  {
+    id: 8,
+    icon: 'ri-secure-payment-line',
+    title: 'make payment',
+    desc: 'Secure your adventure with ease through our streamlined payment process, ensuring a hassle-free booking experiences.',
+  },
+  {
+    id: 9,
+    icon: 'ri-flight-takeoff-line',
+    title: 'reach airport on selected date',
+    desc: 'Prepare for takeoff as you finalize your travel plans, arriving at the airport on your selected date ready to embark on your next unforgettable journey.',
+  },
+]
+
+function displayTrip(trip) {
+  let displayTripItem = trip
+    .map(function (item) {
+      return `
+    <li id='${item.id}'>
+      <div class="trip_li">
+        <span><i class="${item.icon}"></i></span>
+        <div className="trip_condition">
+          <h4>${item.title}</h4>
+          <p>${item.desc}</p>
+        </div>
+      </div>
+    </li>
+    `
+    })
+    .join('')
+  tripList.innerHTML = displayTripItem
+}
+displayTrip(tripArray)
+
+//** ScrollReveal  **/
+
+//: Header
 sr.reveal('.header_image img', {
   ...ScrollRevealOption,
   origin: 'right',
@@ -43,17 +188,17 @@ sr.reveal('.header_btns', {
   ...ScrollRevealOption,
   delay: 1300,
 })
-//: Service 
+//: Service
 sr.reveal('.service_card', {
   ...ScrollRevealOption,
   interval: 300,
 })
-//: Destination 
+//: Destination
 sr.reveal('.destination_card', {
   ...ScrollRevealOption,
   interval: 300,
 })
-//: Trip 
+//: Trip
 sr.reveal('.trip_image img', {
   ...ScrollRevealOption,
   origin: 'right',
@@ -66,28 +211,25 @@ sr.reveal('.trip_content h2', {
   ...ScrollRevealOption,
   delay: 700,
 })
-
 sr.reveal('.trip_list li', {
   ...ScrollRevealOption,
   delay: 1000,
-  interval: 300
+  interval: 300,
 })
-//: Client 
+//: Client
 sr.reveal('#client h3', {
   ...ScrollRevealOption,
 })
-
 sr.reveal('#client h2', {
   ...ScrollRevealOption,
   delay: 300,
 })
-
 const swiper = new Swiper('.swiper', {
-  direction:'vertical',
-  autoHeight:true,
-  SlidesPerView:1,
+  direction: 'vertical',
+  autoHeight: true,
+  SlidesPerView: 1,
 })
-//: Client 
+//: Client
 sr.reveal('.footer_col', {
   ...ScrollRevealOption,
   interval: 300,
--- todo.md
+++ todo.md
@@ -1,19 +1,9 @@
-
-
-## Javascript 
-add JSon files
-
 # File
-resize img
 delete usefull file
 
-
-# Check W3C
-- HTML
-- CSS
-upload online
 
 # Readme
 add description
 tool used
 img
+upload online
